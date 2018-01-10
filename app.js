379f2d41828bcdafaa8e76ca4184cd97d073afcb
Przemek Fijak <przemaslaw12@gmail.com>
2018-01-10 09:56:38 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+function clock() {
+  const fullDate = new Date();
+
+  //pobranie bierzących wartości daty
+  var hours = fullDate.getHours();
+  var mins = fullDate.getMinutes();
+  var secs = fullDate.getSeconds();
+
+
+  //dodanie zera
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (mins < 10) {
+    mins = "0" + mins;
+  }
+  if (secs < 10) {
+    secs = "0" + secs;
+  }
+
+
+  //zaktualizowanie daty
+  document.getElementById('hour').innerHTML = hours + ":";
+  document.getElementById('minute').innerHTML = mins + ":";
+  document.getElementById('second').innerHTML = secs;
+}
+
+//pętla do odświeżania daty
+setInterval(clock, 100);
